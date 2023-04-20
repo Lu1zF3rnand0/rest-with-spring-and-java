27c4083dcc1dc6d2406800dc0734b27c71e31ea4
Luiz Fernando <luizfernando.silva@outlook.com>
2023-04-20 19:54:08 -0300

    Some configuration tweaks.

--- src/main/java/com/luiz/Startup.java
+++ src/main/java/com/luiz/Startup.java
@@ -1,13 +1,13 @@
-package com.luiz.restwithspringandjava;
+package com.luiz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RestWithSpringAndJavaApplication {
+public class Startup {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RestWithSpringAndJavaApplication.class, args);
+		SpringApplication.run(Startup.class, args);
 	}
 
 }
